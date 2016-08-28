01c6ef985209a215a90b4817a1aeb19698d91259
Jason Neal <jason.neal@astro.up.pt>
2016-08-29 00:07:08 +0100

    Create Class and initalize

---
+++ Spectrum.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from __future__ import print_function, division
+
+# Spectrum Class
+
+# Begun August 2016
+# Jason Neal
+
+
+class Spectrum:
+    """ Spectrum class represents and manipulates astronomical spectra. """
+    
+    def __init__(self, pixel=[], flux=[], wavelength=[]):
+        """ Create a empty spectra """
+        self.pixel = pixel
+        self.flux = flux
+        self.wavelength = wavelength
+
