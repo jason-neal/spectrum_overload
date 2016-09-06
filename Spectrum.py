404df06628ae9606bf6b597184528fe403cb15bc
Jason Neal <jason.neal@astro.up.pt>
2016-09-06 23:12:36 +0100

    Add wavelength selection

--- Spectrum.py
+++ Spectrum.py
@@ -16,7 +16,26 @@ class Spectrum:
         self.flux = flux
         self.calibrated = calibrated
 
-
+    def wav_select(self, wav_min, wav_max):
+        """ Fast Wavelength selector between wav_min and wav_max values 
+        If passed lists it will return lists.
+        If passed np arrays it will return arrays
+    
+        """
+        if isinstance(self.xaxis, list): # If passed lists
+            wav_sel = [wav_val for wav_val in self.xaxis if (wav_min < wav_val < wav_max)]
+            flux_sel = [flux_val for wav_val, flux_val in zip(self.xaxis, self.flux) if (wav_min < wav_val < wav_max)]
+        elif isinstance(self.xaxis, np.ndarray):
+            # Super Fast masking with numpy
+            mask = (self.xaxis > wav_min) & (self.xaxis < wav_max)
+            wav_sel = self.xaxis[mask]
+            flux_sel = self.flux[mask]
+        
+        else:
+              raise TypeError("Unsupported input wav type of type ", type(self.xaxis))
+        # Set new spectra
+        self.xaxis = wav_sel
+        self.flux = flux_sel
 
 
 # Try using Spectrum 
