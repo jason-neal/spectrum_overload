4d01a714868c10eabef41b125e719d2c7f84fbf7
Jason Neal <jason.neal@astro.up.pt>
2016-09-10 00:12:17 +0100

    Update of many functions (ENAA2016) (#1)
    * Wav select test example.
    
    Test with hypothesis that it moves in correct direction.
    
    * Automatically turn to arrays
    
    * Remove separate implementation for list and ndarrays.
    
    Make a comment if a TypeError is encountered
    
    * Add simple doc strings to tests
    
    * Apply calibration from wavelength mapping polynomial parameters.
    
    * Update git ignore with ipynb and html
    
    * Shell of function for interpolation
    
    * Script to run install and test together
    
    Need to make a similar for windows (batch script?)
    
    * Fix misspelling of xaxis in calibration function
    
    * Simple linear test of calibration function.
    
    Use `np.allclose()` to deal with floating point errors. Can add a desired
    tolerance
    
    * Warning if wavelength solution less or equal to zero.
    
    * Add warning about very small RV shifts ( <1 mm/s)
    
    * Change name of test function
    
    (Test is failing at the moment)
    
    * Adjusting hypothesis parameters to take in the xaxis also
    
    * Change to not perform Doppler shift if  less than 0.1mm/s. Change warning text
    
    * Catch RV values of inf and Nan.
    
    Doesn't perform doppler shift and returns a warning.
    
    * Add tolerance for small rv values 1 mm/s
    
    replace `all()` with `np.all()`
    
    * Make float64, decrease tolerance to 1e-5, fix space typo
    
    * Add another `allclose` and add extra constraints on float inputs
    
    * Revert "Make float64, decrease tolerance to 1e-5, fix space typo"
    
    This reverts commit 8c04bb1509d1769c28c3e8382ec48347fffadc4e.
    
    * Fix raise indent error
    
    * Can't select from an attribute with length zero
    
    * Add `np.all()` to asserts to catch all asarray == list.
    
    * Remove one hypothesis list so that flux and xaxis are same length
    
    * Directly reassign attributes in wav_select. Restore originals if it fails in the try.
    
    * Remove nan and inf from assignment test
    
    * Fixed the culprit. `np.isnan` and `np.isinf`
    
    Now all tests pass.
    
    * Relax constraints on Doppler shifting. Test nan and inf.
    
    * Don't know what happened there. this change got missed from last commit
    
    * Add some doc strings to tests and make alignments to >80 characters
    
    * Comment about interpolation from ENAA

--- test/test_Spectrum.py
+++ test/test_Spectrum.py
@@ -2,6 +2,7 @@
 
 from __future__ import division, print_function
 import pytest
+import numpy as np
 
 import sys
 # Add Spectrum location to path
@@ -12,39 +13,105 @@ import Spectrum
 from hypothesis import given
 import hypothesis.strategies as st
 
-@given(st.lists(st.floats()), st.lists(st.floats()), st.booleans())
+@given(st.lists(st.floats(allow_infinity=False, allow_nan=False)), 
+    st.lists(st.floats(allow_infinity=False, allow_nan=False)), st.booleans())
 def test_spectrum_assigns_hypothesis_data(y, x, z):
-
+    """Test that data was assigned to the correct attributes"""
     spec = Spectrum.Spectrum(y, x, z)
-    assert spec.flux == y
-    assert spec.xaxis == x
+    assert np.all(spec.flux == y)
+    assert np.all(spec.xaxis == x)
     assert spec.calibrated == z
 
 def test_spectrum_assigns_data():
-
-    x = [1,2,3,4,5,6]
-    y = [1,1,0.9,0.95,1,1]
+    """Test a manual example"""
+    x = [1, 2, 3, 4, 5, 6]
+    y = [1, 1, 0.9, 0.95, 1, 1]
     calib_val = 0
 
     spec = Spectrum.Spectrum(y, x, calibrated=calib_val)
-    assert spec.flux == y
-    assert spec.xaxis == x
+    assert np.all(spec.flux == y)
+    assert np.all(spec.xaxis == x)
     assert spec.calibrated == calib_val
 
-
-@given(st.lists(st.floats()), st.lists(st.floats()), st.booleans(), st.floats(), st.floats())
-def test_wav_select(y, x, calib, wav_min, wav_max):
+@given(st.lists(st.floats()), st.booleans(), st.floats(), st.floats())
+def test_wav_select(x, calib, wav_min, wav_max):
+    """Test some properties of wavelength selection"""
     # Create specturm
+    y = np.copy(x)
     spec = Spectrum.Spectrum(y, xaxis=x, calibrated=calib)
     # Select wavelength values
     spec.wav_select(wav_min, wav_max)
 
-    # All values in selected spectrum should be less than the max and greater than the min value.
+    # All values in selected spectrum should be less than the max and greater
+    # than the min value.
     if isinstance(spec.xaxis, list):
         assert all([xval >= wav_min for xval in spec.xaxis])
         assert all([xval <= wav_max for xval in spec.xaxis])
     else:
         assert all(spec.xaxis >= wav_min)
         assert all(spec.xaxis <= wav_max)
+
+def test_wav_select_example():
+    """Manual test of a wavelength selection"""
+    # Create specturm
+    y = 2*np.random.random(20)
+    x = np.arange(20)
+    calib = False
+    spec = Spectrum.Spectrum(y, xaxis=x, calibrated=calib)
+    # Select wavelength values
+
+    spec.wav_select(5, 11)
+    # All values in selected spectrum should be less than the max and greater
+    # than the min value.
+    assert all(spec.xaxis >= 5)
+    assert all(spec.xaxis <= 11)
+    assert all(spec.xaxis == np.arange(6, 11))
+    assert all(spec.flux == y[np.arange(6, 11)])
+
+
     ##Also need to test asignment!
     # spec2 = spec.wav_selector()
+@given(st.lists(st.floats(min_value=1e-6, allow_infinity=False), min_size=1),
+       st.floats(), st.booleans())
+def test_doppler_shift_with_hypothesis(x, RV, calib):
+    """Test doppler shift properties. 
+    Need to check values against pyastronomy separately """
+    x = np.asarray(x)
+    y = np.random.random(len(x))
+
+    spec = Spectrum.Spectrum(y, x, calib)
+    # Apply Doppler shift of RV km/s.
+    spec.doppler_shift(RV)
+
+    if not spec.calibrated:
+        assert np.allclose(spec.xaxis, x)
+    else:
+        tolerance = 1e-6
+        if abs(RV) < tolerance:
+            assert np.allclose(spec.xaxis, x)
+        elif np.isnan(RV) or np.isinf(RV):
+            assert np.all(spec.xaxis == x)
+        elif RV < 0:
+            assert np.all(spec.xaxis < x)
+        elif RV > 0:
+            assert np.all(spec.xaxis > x)
+
+
+def test_x_calibration_works():
+    """ Simple test to check that the calibration works """
+    "Setup the code "
+    x = [1,2,3,4,5,6,7,8,9,10]
+    x = [float(x_i) for x_i in x]
+    y = np.ones_like(x)
+    spec = Spectrum.Spectrum(y, x, False)
+
+    #Easy test
+    params = np.polyfit([1,5,10], [3,15,30], 1)
+
+    spec.calibrate_with(params)
+
+    assert spec.calibrated
+    assert np.allclose(spec.xaxis, np.asarray(x)*3)
+
+
+
