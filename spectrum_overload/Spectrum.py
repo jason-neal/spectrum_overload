64d0e6a444ad1caa0ef979b158f860f049351be2
Jason Neal <jason.neal@astro.up.pt>
2016-10-13 15:58:58 +0100

    Update documentation for spline interpolation

--- spectrum_overload/Spectrum.py
+++ spectrum_overload/Spectrum.py
@@ -327,51 +327,60 @@ class Spectrum(object):
 
     def spline_interpolate_to(self, reference, w=None, bbox=[None, None], k=3,
                               ext=0, check_finite=False, bounds_error=False):
-        """Interpolate wavelength solution to the reference wavelength using 
-        InterpolatedUnivariateSpline.
-        Using scipy interpolation so the optional parameters are passed to
-        scipy.
-        See scipy.interolate.InterpolatedUnivariateSpline for more details
-        https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.interpolate.InterpolatedUnivariateSpline.html#scipy.interpolate.InterpolatedUnivariateSpline
-        
-        Documentation copied from Sicpy:
+        """ Interpolate wavelength solution using scipy's
+                InterpolatedUnivariateSpline.
+
+        The optional parameters are for scipy's InterpolatedUnivariateSpline
+        function.
+
+        Documentation copied from Sicpy
         One-dimensional interpolating spline for a given set of data points.
 
         Fits a spline y = spl(x) of degree k to the provided x, y data. Spline
         function passes through all provided points. Equivalent to
         UnivariateSpline with s=0.
-        Parameters: 
+
+        Parameters
+        ----------
         x : (N,) array_like
-        Input dimension of data points – must be increasing
-
+            Input dimension of data points this much be must be in
+            ascending order.
         y : (N,) array_like
-        input dimension of data points
-
+            Input dimension of data points
         w : (N,) array_like, optional
-        Weights for spline fitting. Must be positive. If None (default),
-        weights are all equal.
-
+            Weights for spline fitting.
+            Must be positive. If None (default), weights are all equal.
         bbox : (2,) array_like, optional
-        2-sequence specifying the boundary of the approximation interval.
-        If None (default), bbox=[x[0], x[-1]].
-
+            2-sequence specifying the boundary of the approximation
+            interval. If None (default), bbox=[x[0], x[-1]].
         k : int, optional
-        Degree of the smoothing spline. Must be 1 <= k <= 5.
-
+            Degree of the smoothing spline. Must be 1 <= k <= 5.
         ext : int or str, optional
-        Controls the extrapolation mode for elements not in the interval
-        defined by the knot sequence.
-            if ext=0 or ‘extrapolate’, return the extrapolated value.
-            if ext=1 or ‘zeros’, return 0
-            if ext=2 or ‘raise’, raise a ValueError
-            if ext=3 of ‘const’, return the boundary value.
-        The default value is 0.
-    
+            Controls the extrapolation mode for elements not in the
+            interval defined by the knot sequence.
+                if ext=0 or ‘extrapolate’, return the extrapolated value.
+                if ext=1 or ‘zeros’, return 0
+                if ext=2 or ‘raise’, raise a ValueError
+                if ext=3 of ‘const’, return the boundary value.
+            Default value is 0.
         check_finite : bool, optional
-        Whether to check that the input arrays contain only finite numbers.
-        Disabling may give a performance gain, but may result in problems
-        (crashes, non-termination or non-sensical results) if the inputs do
-        contain infinities or NaNs. Default is False.
+            Whether to check that the input arrays contain only
+            finite numbers. Disabling may give a performance gain,
+            but may result in problems (crashes, non-termination
+            or non-sensical results) if the inputs do contain
+            infinities or NaNs.
+            Default is False.
+
+        Raises:
+        -------
+        TypeError:
+            Cannot interpolate with the given object of type
+        ValueError:
+            A value in reference is outside the interpolation range."
+
+        See also
+        --------
+            https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.interpolate.InterpolatedUnivariateSpline.html#scipy.interpolate.InterpolatedUnivariateSpline
 
         """
 
