bd80cb15fb161e9797a40fdee8524faf27c298dc
Jason Neal <jason.neal@astro.up.pt>
2017-04-24 00:43:13 +0100

    Add numpy to setup_requires.
    To resolve issue with pyastronomy.

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['numpy', 'hypothesis', 'astropy', 'scipy'],
-    setup_requires=['pytest-runner'],
+    setup_requires=['numpy','pytest-runner'],
     tests_require=['pytest'],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
