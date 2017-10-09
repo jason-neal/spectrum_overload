0e2b89733dc184bee0b7a5e33027391dc17d001a
Jason Neal <jason.neal@astro.up.pt>
2017-10-09 17:59:33 +0100

    Replace kwargs `top` and `splits` with `ntop` and `bins`.

--- spectrum_overload/norm.py
+++ spectrum_overload/norm.py
@@ -2,39 +2,39 @@
 import numpy as np
 
 
-def get_continuum_points(wave, flux, splits=50, top=20):
+def get_continuum_points(wave, flux, nbins=50, ntop=20):
     """Get continuum points along a spectrum.
 
-    This splits a spectrum into "splits" number of bins and calculates
-    the median wavelength and flux of the upper "top" number of flux
+    This splits a spectrum into "nbins" number of bins and calculates
+    the median wavelength and flux of the upper "ntop" number of flux
     values.
     """
     # Shorten array until can be evenly split up.
-    remainder = len(flux) % splits
+    remainder = len(flux) % nbins
     if remainder:
         # Non-zero remainder needs this slicing
         wave = wave[:-remainder]
         flux = flux[:-remainder]
 
-    wave_shaped = wave.reshape((splits, -1))
-    flux_shaped = flux.reshape((splits, -1))
+    wave_shaped = wave.reshape((nbins, -1))
+    flux_shaped = flux.reshape((nbins, -1))
 
-    s = np.argsort(flux_shaped, axis=-1)[:, -top:]
+    s = np.argsort(flux_shaped, axis=-1)[:, -ntop:]
 
     s_flux = np.array([ar1[s1] for ar1, s1 in zip(flux_shaped, s)])
     s_wave = np.array([ar1[s1] for ar1, s1 in zip(wave_shaped, s)])
 
     wave_points = np.median(s_wave, axis=-1)
     flux_points = np.median(s_flux, axis=-1)
-    assert len(flux_points) == splits
+    assert len(flux_points) == nbins
 
     return wave_points, flux_points
 
 
-def continuum(wave, flux, splits=50, method='scalar', top=20, degree=None):
+def continuum(wave, flux, nbins=50, method='scalar', ntop=20, degree=None):
     """Fit continuum of flux.
 
-    top: is number of top points to take median of continuum.
+    ntop: is number of top points to take median of continuum.
     """
     if method == "poly" and degree is None:
         raise ValueError("No degree specified for continuum method 'poly'.")
@@ -45,7 +45,7 @@ def continuum(wave, flux, splits=50, method='scalar', top=20, degree=None):
     org_wave = wave[:]
 
     # Get continuum value in chunked sections of spectrum.
-    wave_points, flux_points = get_continuum_points(wave, flux, splits=splits, top=top)
+    wave_points, flux_points = get_continuum_points(wave, flux, nbins=nbins, ntop=ntop)
 
     poly_degree = {"scalar": 0, "linear": 1, "quadratic": 2, "cubic": 3, "poly": degree}
 
