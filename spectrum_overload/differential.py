94ae0232c5b2bc79943be2bda225edd904aa34a4
Jason Neal <jason.neal@astro.up.pt>
2017-12-10 00:23:18 +0000

    Tidy/fix up differential.py as it is so far.

--- spectrum_overload/differential.py
+++ spectrum_overload/differential.py
@@ -3,6 +3,7 @@
 """Differential Class which takes the difference between two spectra."""
 from spectrum_overload import Spectrum
 
+
 # Begin Feburary 2017
 # Jason Neal
 
@@ -12,26 +13,28 @@ from spectrum_overload import Spectrum
 class DifferentialSpectrum(object):
     """A differential spectrum."""
 
-        def check_compatibility(spec1, spec2):
-            """Check spectra are compatible to take differences.
+    def check_compatibility(spec1, spec2):
+        """Check spectra are compatible to take differences.
 
-            Requires most of the setting to be the same. Have included a CRIRES only parameters also.
-            """
-            compatible = True
-            for check in ["EXPTIME", "HIERARCH ESO INS SLIT1 WID", "OBJECT"]:
-                if spec1.header[check] != spec1.header[check]:
-                    print("The Spectral property '{}' are not compatible. {}, {}".format(check, spec1.header[check], spec2.header[check]))
-                    compatible = False
-            return compatible
+        Requires most of the setting to be the same. Have included a CRIRES only parameters also.
+        """
+        compatible = True
+        for check in ["EXPTIME", "HIERARCH ESO INS SLIT1 WID", "OBJECT"]:
+            if spec1.header[check] != spec1.header[check]:
+                print("The Spectral property '{}' are not compatible. {}, {}".format(check, spec1.header[check],
+                                                                                     spec2.header[check]))
+                compatible = False
+        return compatible
 
-    def __init__(self, Spectrum1, Spectrum2, params=None):
-        """Initalise lass with both spectra."""
-        if not(Spectrum1.calibrated and Spectrum2.calibrated):
+    def __init__(self, spectrum1, spectrum2, params=None):
+        """Initialise lass with both spectra."""
+        assert isinstance(spectrum1, Spectrum) and isinstance(spectrum2, Spectrum)
+        if not (spectrum1.calibrated and spectrum2.calibrated):
             raise ValueError("Input spectra are not calibrated.")
 
-        if check_compatibility(spec1, spec2):
-            self.spec1 = Spectrum1
-            self.spec2 = Spectrum2
+        if self.check_compatibility(spectrum1, spectrum2):
+            self.spec1 = spectrum1
+            self.spec2 = spectrum2
             self.diff = None
         else:
             raise ValueError("The spectra are not compatible.")
@@ -46,18 +49,17 @@ class DifferentialSpectrum(object):
         # TODO: Turn into datetime?
         return (time, time2)
 
-
     def rest_frame(self, frame):
         """Change restframe to one of the spectra."""
         pass
 
     def diff(self):
         """Calculate difference between the two spectra."""
-        if check_compatibility(self.spec1, self.spec2):
+        if self.check_compatibility(self.spec1, self.spec2):
             return self.spec1 - self.spec2
         else:
             raise ValueError("The spectra are not compatible.")
-        # TODO: Access interpolations
+            # TODO: Access interpolations
 
     def sort(self, method="time"):
         """Sort spectra in specific order. e.g. time, reversed."""
---
+++ spectrum_overload/spec_diff.py
@@ -0,0 +1,50 @@
+import argparse
+import sys
+
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
+from spectrum_overload import Spectrum, DifferentialSpectrum
+
+
+def parse_args(args):
+    """Take care of all the argparse stuff.
+
+    :returns: the args
+    """
+    parser = argparse.ArgumentParser(description='Spectrum Difference.')
+    parser.add_argument('spectrum1', help='First Spectrum')
+    parser.add_argument('spectrum2', help="Second Spectrum")
+    parser.add_argument('params', help="Orbital parameter file.")
+    parser.add_argument("-p", '--plot', help="Plot differential")
+    return parser.parse_args(args)
+
+
+def load_spectrum(fname):
+    data = fits.getdata(fname)
+    header = fits.getheader(fname)
+
+    return Spectrum(xaxis=data["wavelength"], flux=data["flux"], header=header)
+
+
+def main(spectrum1, spectrum2, params=None, plot=True):
+    spec1 = load_spectrum(spectrum1)
+    spec2 = load_spectrum(spectrum2)
+
+    # params = load/parse parameter file
+
+    diff = DifferentialSpectrum(spec1, spec2, params=params)
+
+    if plot():
+        diff.plot()
+        plt.show()
+
+    print("Done")
+
+
+if __name__ == '__main__':
+    args = vars(parse_args(sys.argv[1:]))
+    opts = {k: args[k] for k in args}
+
+    result = main(**opts)
+    sys.exit(result)
